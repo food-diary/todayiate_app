a53bee2d7257e19fc367ddfbeb06a591602a9091
thumbsu <kisum.js@gmail.com>
2019-10-27 19:11:57 +0900

    react-native-inappbrowser

--- app/screens/welcome-screen/welcome-screen.tsx
+++ app/screens/welcome-screen/welcome-screen.tsx
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, TextStyle, Linking } from "react-native"
+import { ViewStyle, TextStyle, Linking, Alert } from "react-native"
 import { Text } from "../../components/text"
 import { Screen } from "../../components/screen"
 // import { useStores } from "../../models/root-store"
 import { color, spacing } from "../../theme"
 import { NavigationStackScreenProps } from "react-navigation-stack"
 import { Button } from "../../components/button"
+import InAppBrowser from "react-native-inappbrowser-reborn"
 
 export interface WelcomeScreenProps extends NavigationStackScreenProps<{}> {}
 
@@ -48,6 +49,19 @@ const HOME_TEXT: TextStyle = {
   fontSize: 18,
 }
 
+const openLink = async () => {
+  try {
+    const url = 'https://www.google.com'
+    if (await InAppBrowser.isAvailable()) {
+      const result = await InAppBrowser.open(url)
+      Alert.alert(JSON.stringify(result))
+    }
+    else Linking.openURL(url)
+  } catch (error) {
+    Alert.alert(error.message)
+  }
+}
+
 export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = observer(props => {
   const { navigation } = props
   // const { someStore } = useStores()
@@ -55,11 +69,7 @@ export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = observ
     <Screen style={ROOT}>
       <Text preset="header" text="식사일기에 오신 것을 환영합니다." style={HEADER_TEXT} />
       <Button
-        onPress={() =>
-          Linking.openURL(
-            "https://todayiate-dev.auth.ap-northeast-2.amazoncognito.com/login?response_type=code&client_id=1f110cfnepcfuar1327kckd6f0&redirect_uri=http://localhost/",
-          )
-        }
+        onPress={() => openLink()}
         style={GO_TO_SIGNUP}
         children={<Text text="구글 계정으로 로그인" style={SIGNUP_TEXT} />}
       />
